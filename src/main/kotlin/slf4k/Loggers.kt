e46cf1c8337f39abe13b1ba71d907a8d3fea0f82
Nicholas Yip <nicholas.yip@justincase-tech.com>
2019-11-27 00:38:27 +0900

    Capitalize method names

--- src/main/kotlin/slf4k/Loggers.kt
+++ src/main/kotlin/slf4k/Loggers.kt
@@ -1,3 +1,5 @@
+@file:Suppress("FunctionName")
+
 package slf4k
 
 import org.slf4j.LoggerFactory
@@ -7,31 +9,31 @@ internal inline val logger
   get() = LoggerFactory.getLogger(Thread.currentThread().stackTrace[1].className.substringAfterLast('.'))
 
 
-inline fun trace(throwable: Throwable? = null, message: () -> Any?) = logger.run {
+inline fun TRACE(throwable: Throwable? = null, message: () -> Any?) = logger.run {
   if (isTraceEnabled) {
     trace(message().toString(), throwable)
   }
 }
 
-inline fun debug(throwable: Throwable? = null, message: () -> Any?) = logger.run {
+inline fun DEBUG(throwable: Throwable? = null, message: () -> Any?) = logger.run {
   if (isDebugEnabled) {
     debug(message().toString(), throwable)
   }
 }
 
-inline fun info(throwable: Throwable? = null, message: () -> Any?) = logger.run {
+inline fun INFO(throwable: Throwable? = null, message: () -> Any?) = logger.run {
   if (isInfoEnabled) {
     info(message().toString(), throwable)
   }
 }
 
-inline fun warn(throwable: Throwable? = null, message: () -> Any?) = logger.run {
+inline fun WARN(throwable: Throwable? = null, message: () -> Any?) = logger.run {
   if (isWarnEnabled) {
     warn(message().toString(), throwable)
   }
 }
 
-inline fun error(throwable: Throwable? = null, message: () -> Any?) = logger.run {
+inline fun ERROR(throwable: Throwable? = null, message: () -> Any?) = logger.run {
   if (isErrorEnabled) {
     error(message().toString(), throwable)
   }
