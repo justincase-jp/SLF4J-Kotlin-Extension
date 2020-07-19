1b1a9c30da063e8a1b2be8fceb3b77db545a500e
Nicholas Yip <nicholas.yip@justincase-tech.com>
2020-07-19 10:25:23 +0900

    Support markers

--- src/main/kotlin/slf4k/Loggers.kt
+++ src/main/kotlin/slf4k/Loggers.kt
@@ -3,6 +3,7 @@
 package slf4k
 
 import org.slf4j.LoggerFactory
+import org.slf4j.Marker
 
 @PublishedApi
 internal inline val logger
@@ -36,5 +37,36 @@ inline fun WARN(throwable: Throwable? = null, message: () -> Any?) = logger.run 
 inline fun ERROR(throwable: Throwable? = null, message: () -> Any?) = logger.run {
   if (isErrorEnabled) {
     error(message().toString(), throwable)
+  }
+}
+
+
+inline fun Marker.TRACE(throwable: Throwable? = null, message: () -> Any?) = logger.run {
+  if (isTraceEnabled(this@TRACE)) {
+    trace(this@TRACE, message().toString(), throwable)
+  }
+}
+
+inline fun Marker.DEBUG(throwable: Throwable? = null, message: () -> Any?) = logger.run {
+  if (isDebugEnabled(this@DEBUG)) {
+    debug(this@DEBUG, message().toString(), throwable)
+  }
+}
+
+inline fun Marker.INFO(throwable: Throwable? = null, message: () -> Any?) = logger.run {
+  if (isInfoEnabled(this@INFO)) {
+    info(this@INFO, message().toString(), throwable)
+  }
+}
+
+inline fun Marker.WARN(throwable: Throwable? = null, message: () -> Any?) = logger.run {
+  if (isWarnEnabled(this@WARN)) {
+    warn(this@WARN, message().toString(), throwable)
+  }
+}
+
+inline fun Marker.ERROR(throwable: Throwable? = null, message: () -> Any?) = logger.run {
+  if (isErrorEnabled(this@ERROR)) {
+    error(this@ERROR, message().toString(), throwable)
   }
 }
